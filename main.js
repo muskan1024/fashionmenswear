9bda29ef515898b77353a8d3c7e82472d309288e
shaikhmuskan1024@gmail.com <shaikhmuskan1024@gmail.com>
2022-07-24 22:44:56 +0530

    update1

--- main.js
+++ main.js
@@ -46,3 +46,27 @@ function togglehide1() {
     // clockButton.innerHTML = 'Hide Time';
     }
 }
+function togglehide2() {
+    // get the clock
+    var myClock = document.getElementById('time2');
+
+    // get the current value of the clock's display property
+    var displaySetting = myClock.style.display;
+
+    // also get the clock button, so we can change what it says
+    var clockButton = document.getElementById('togglehide2');
+
+    // now toggle the clock and the button text, depending on current state
+    if (displaySetting == 'block') {
+    // clock is visible. hide it
+    myClock.style.display = 'none';
+    // change button text
+    clockButton.innerHTML = 'Contact Us:';
+    }
+    else {
+    // clock is hidden. show it
+    myClock.style.display = 'block';
+    // change button text
+    // clockButton.innerHTML = 'Hide Time';
+    }
+}
